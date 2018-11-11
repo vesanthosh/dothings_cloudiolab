e280fed83cfc4fb4354a3534a09292c89334e7bf
Santhosh Venkatesan <vesanthosh@gmail.com>
2018-11-11 15:13:43 +0100

    Application Ready for Production

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.use("/api/items", items);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+
   // Set statci folder
   app.use(express.static('client/build'));
 
