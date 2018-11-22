ea7706206f431ef7ae7cff4480402b24f62c30fe
Santhosh Venkatesan <vesanthosh@gmail.com>
2018-11-22 18:15:27 +0100

    url change

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://admin:admin1234@ds159293.mlab.com:59293/to_do_db" // Production URL from mLab.
-  // mongoURI: "mongodb://admin:secret@127.0.0.1:27017/admin"
+  // mongoURI: "mongodb://admin:root@127.0.0.1:27017/admin" // Home URL
+  // mongoURI: "mongodb://admin:secret@127.0.0.1:27017/admin" // Office URL
 };
