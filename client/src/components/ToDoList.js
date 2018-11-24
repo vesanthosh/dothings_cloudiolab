a167de315108663bdd6073b3eb36c9cc22a42606
Santhosh Venkatesan <vesanthosh@gmail.com>
2018-11-24 15:30:07 +0100

    - implemented update feature

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -4,6 +4,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
+import UpdateItemModal from './UpdateItemModal';
 
 class ToDoList extends Component {
 
@@ -23,14 +24,17 @@ class ToDoList extends Component {
                     <TransitionGroup className="todo-list">
                         {items.map(({ _id, name, description }) => (
                             <CSSTransition key={_id} timeout={500} classNames="fade">
-                                <ListGroupItem>
+                                <ListGroupItem className="todo-content">
+                                    {name} | {description}
+                                    <div className="group-content">
+                                        <UpdateItemModal _id={_id} name={name} description={description} />
                                     <Button
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
                                         onClick={this.onDeleteClick.bind(this, _id)}>
-                                        &#10008;</Button>
-                                    {name} | {description}</ListGroupItem>
+                                            &#10008;</Button></div>
+                                </ListGroupItem>
                             </CSSTransition>
                         ))}
                     </TransitionGroup>
