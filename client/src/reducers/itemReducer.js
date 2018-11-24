14348b268836e548deb126d0451fe820e1e93833
Santhosh Venkatesan <vesanthosh@gmail.com>
2018-11-24 15:31:35 +0100

    - added UPDATE_ITEM reducer

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,4 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
     items: [],
@@ -14,6 +14,11 @@ export default function (state = initialState, action) {
                 loading: false // Disablling loading spinner once we get all the data.
             };
         case ADD_ITEM:
+            return {
+                ...state,
+                items: [...state.items, action.payload]
+            };
+        case UPDATE_ITEM:
             return {
                 ...state,
                 items: [...state.items, action.payload]
