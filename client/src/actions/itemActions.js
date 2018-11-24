fa99f6612b270163cd787b52eb1dc80973ab222b
Santhosh Venkatesan <vesanthosh@gmail.com>
2018-11-24 15:27:45 +0100

    - added updateItem

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,4 +1,4 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
+import { GET_ITEMS, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 import axios from 'axios';
 
 export const getItems = () => dispatch => {
