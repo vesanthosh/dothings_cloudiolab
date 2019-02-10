a36af93b378aa2094086cc1554c4af16b2cc655b
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-02-10 21:44:44 +0100

    - added item action: addTodoItem

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,41 +1,74 @@
-import { GET_ITEMS, ADD_ITEM, UPDATE_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
+import { GET_ITEMS, ADD_ITEM, ITEMS_LOADING, GET_ERRORS } from './types';
 import axios from 'axios';
 
-export const getItems = () => dispatch => {
-    dispatch(setItemsLoading());
-    axios.get('/api/items')
-        .then(res => dispatch({
-            type: GET_ITEMS,
-            payload: res.data
+// Get list of todo items
+export const getTodoItems = () => dispatch => {
+    dispatch(setItemLoading());
+    axios.get('/api/todoItem/all')
+        .then(res =>
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            }))
+        .catch(err =>
+            dispatch({
+                type: GET_ITEMS,
+                payload: err.response.data
+            }));
+}
+
+export const addTodoItem = (itemData, history) => dispatch => {
+    axios.post('/api/todoItem', itemData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
         }));
 };
 
-export const addItem = (item) => dispatch => {
-    axios.post('/api/items', item)
-        .then(res => dispatch({
-            type: ADD_ITEM,
-            payload: res.data
-        }));
-};
 
-export const updateItem = (item) => dispatch => {
-    axios.put(`/api/items/${item._id}`, item)
-        .then(res => dispatch({
-            type: UPDATE_ITEM,
-            payload: res.data
-        }));
-};
+// export const getItems = () => dispatch => {
+//     dispatch(setItemsLoading());
+//     axios.get('/api/items')
+//         .then(res => dispatch({
+//             type: GET_ITEMS,
+//             payload: res.data
+//         }));
+// };
 
-export const deleteItem = (_id) => dispatch => { // This _id could be anything.
-    axios.delete(`/api/items/${_id}`)
-        .then(res => dispatch({
-            type: DELETE_ITEM,
-            payload: _id
-        }));
-};
+// export const addItem = (item) => dispatch => {
+//     axios.post('/api/items', item)
+//         .then(res => dispatch({
+//             type: ADD_ITEM,
+//             payload: res.data
+//         }));
+// };
 
-export const setItemsLoading = () => {
+// export const updateItem = (item) => dispatch => {
+//     axios.put(`/api/items/${item._id}`, item)
+//         .then(res => dispatch({
+//             type: UPDATE_ITEM,
+//             payload: res.data
+//         }));
+// };
+
+// export const deleteItem = (_id) => dispatch => { // This _id could be anything.
+//     axios.delete(`/api/items/${_id}`)
+//         .then(res => dispatch({
+//             type: DELETE_ITEM,
+//             payload: _id
+//         }));
+// };
+
+// export const setItemsLoading = () => {
+//     return {
+//         type: ITEMS_LOADING
+//     };
+// };
+
+// Profile loading
+export const setItemLoading = () => {
     return {
         type: ITEMS_LOADING
-    };
-};
+    }
+}
