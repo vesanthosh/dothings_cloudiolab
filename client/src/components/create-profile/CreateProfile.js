69ccce78264052d1566b553b0c4979542d0c4530
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-02-14 16:59:56 +0100

    - added back button

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import TextFieldGroup from '../common/TextFieldGroup';
 // import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
@@ -103,6 +103,7 @@ class CreateProfile extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
+                            <Link to="/dashboard" className="btn btn-light">Go Back</Link>
                             <h1 className="display-4 text-center">Create Your Profile</h1>
                             <p className="lead text-center">
                                 Let's get some information to make your profile stand out.
