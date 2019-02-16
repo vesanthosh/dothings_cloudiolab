2f55b132706fb6edf3218b1278096e9536f87c10
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-02-16 16:22:12 +0100

    - refactored delete and add actions

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,4 +1,4 @@
-import { GET_ITEMS, GET_SINGLE_ITEM, ADD_ITEM, ITEMS_LOADING, GET_ERRORS, CLEAR_CURRENT_TODOITEM } from './types';
+import { GET_ITEMS, GET_SINGLE_ITEM, ADD_ITEM, ITEMS_LOADING, GET_ERRORS, CLEAR_CURRENT_TODOITEM, DELETE_ITEM } from './types';
 import axios from 'axios';
 
 // Get list of todo items
@@ -34,9 +34,12 @@ export const getSingleTodoItem = (id) => dispatch => {
 }
 
 // Add todo item
-export const addTodoItem = (itemData, history) => dispatch => {
+export const addTodoItem = (itemData) => dispatch => {
     axios.post('/api/todoItem', itemData)
-        .then(res => history.push('/dashboard'))
+        .then(res => dispatch({
+            type: ADD_ITEM,
+            payload: res.data
+        }))
         .catch(err => dispatch({
             type: GET_ERRORS,
             payload: err.response.data
@@ -47,7 +50,7 @@ export const addTodoItem = (itemData, history) => dispatch => {
 export const deleteTodoItem = (id) => dispatch => {
     axios.delete(`/api/todoItem/${id}`)
         .then(res => dispatch({
-            type: GET_ITEMS,
+            type: DELETE_ITEM,
             payload: res.data
         }))
         .catch(err => dispatch({
