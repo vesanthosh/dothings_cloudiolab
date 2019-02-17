17d3bb7ccccb42a9b49061ad2ecf3c20d4349c56
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-02-17 23:49:52 +0100

    - preparing for production

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb+srv://adminuser5541645:adminuser5541645@clustersandy-xzqyp.mongodb.net/test?retryWrites=true", // Production URL from mLab.
+  mongoURI: "mongodb+srv://snt_do_things:snt_do_things@sntcluster-xzqyp.mongodb.net/test?retryWrites=true", // Production URL from mLab.
   // mongoURI: "mongodb://admin:root@127.0.0.1:27017/to_do_db", // Home URL
   // mongoURI: "mongodb://admin:secret@127.0.0.1:27017/to_do_db", // Office URL
   Key: 'testkey'
