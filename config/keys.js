af451352808794575ac3054b6813a009659d2dba
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-02-17 22:55:01 +0100

    - getting ready for production

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mongoURI: "mongodb://admin:admin1234@ds159293.mlab.com:59293/to_do_db", // Production URL from mLab.
-  mongoURI: "mongodb://admin:root@127.0.0.1:27017/to_do_db", // Home URL
+  mongoURI: "mongodb+srv://adminuser5541645:adminuser5541645@clustersandy-xzqyp.mongodb.net/test?retryWrites=true", // Production URL from mLab.
+  // mongoURI: "mongodb://admin:root@127.0.0.1:27017/to_do_db", // Home URL
   // mongoURI: "mongodb://admin:secret@127.0.0.1:27017/to_do_db", // Office URL
   Key: 'testkey'
 };
