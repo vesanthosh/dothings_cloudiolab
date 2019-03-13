cd21783123d105a2577a8691676f205bc26c45c3
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-03-13 09:43:26 +0100

    - upgrade todo list with new logics

--- client/src/components/todo_item/CompletedTodoItem.js
+++ client/src/components/todo_item/CompletedTodoItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment'; // used to format the date from mongodb
-import { deleteCompletedTodoItem } from '../../actions/itemActions';
+import { deleteCompletedTodoItem, setTodoItemAsNotCompleted } from '../../actions/itemActions';
 
 class CompletedTodoItem extends Component {
 
@@ -10,39 +10,54 @@ class CompletedTodoItem extends Component {
         this.props.deleteCompletedTodoItem(id);
     }
 
+    onChange(id, nameValue, descriptionValue, isCompletedValue) {
+        const newItemData = {
+            name: nameValue,
+            description: descriptionValue,
+            isCompleted: !isCompletedValue
+        };
+        this.props.setTodoItemAsNotCompleted(id, newItemData);
+    }
+
     render() {
-        const completedTodos = this.props.completedTodos.map(completedTodos => ( // TODO: Found profile problems and need to resolve this. till so far not problem
-            <div key={completedTodos._id} className="list-group">
-                <a href="#/" className="list-group-item list-group-item-action flex-column align-items-start">
-                    <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id={completedTodos._id} />
-                                <label className="custom-control-label" htmlFor={completedTodos._id}><del>{completedTodos.name}</del></label>
-                            </div>
-                        </h5>
-                        <small>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <button className="btn btn-danger btn-sm custom-button-width" onClick={this.onDeleteClick.bind(this, completedTodos._id)}>
-                                        <i className="fa fa-trash" />
-                                    </button>
+        var completedTodos;
+        if (this.props.completedTodos.length > 0) {
+            completedTodos = this.props.completedTodos.map(completedTodos => ( // TODO: Found profile problems and need to resolve this. till so far not problem
+                <div key={completedTodos._id} className="list-group">
+                    <a href="#/" className="list-group-item list-group-item-action flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">
+                                <div className="custom-control custom-checkbox">
+                                    <input type="checkbox" className="custom-control-input" id={completedTodos._id} checked={completedTodos.isCompleted} onChange={this.onChange.bind(this, completedTodos._id, completedTodos.name, completedTodos.description, completedTodos.isCompleted)} />
+                                    <label className="custom-control-label" htmlFor={completedTodos._id}><del>{completedTodos.name}</del></label>
                                 </div>
-                            </div>
+                            </h5>
+                            <small>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <button className="btn btn-danger btn-sm custom-button-width" onClick={this.onDeleteClick.bind(this, completedTodos._id)}>
+                                            <i className="fa fa-trash" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </small>
+                        </div>
+                        <p className="mb-1"><del>{completedTodos.description}</del></p>
+                        <small>
+                            <Moment fromNow>{completedTodos.date}</Moment>
                         </small>
-                    </div>
-                    <p className="mb-1"><del>{completedTodos.description}</del></p>
-                    <small>
-                        <Moment fromNow>{completedTodos.date}</Moment>
-                    </small>
-                </a>
-            </div >
-        ));
+                    </a>
+                </div >
+            ));
+        } else {
+            completedTodos = (
+                <p className="mb-1">You have not completed any tasks till so far.</p>
+            );
+        }
 
         return (
             <div className="mb-4">
                 <h4>Tasks Completed</h4>
-                <p className="mb-1">You have no upcoming tasks to do.</p>
                 {completedTodos}
             </div>
         );
@@ -50,7 +65,8 @@ class CompletedTodoItem extends Component {
 }
 
 CompletedTodoItem.propTypes = {
-    deleteCompletedTodoItem: PropTypes.func.isRequired
+    deleteCompletedTodoItem: PropTypes.func.isRequired,
+    setTodoItemAsNotCompleted: PropTypes.func.isRequired
 };
 
-export default connect(null, { deleteCompletedTodoItem })(CompletedTodoItem);
+export default connect(null, { deleteCompletedTodoItem, setTodoItemAsNotCompleted })(CompletedTodoItem);
--- client/src/components/todo_item/UpComingTodoItem.js
+++ client/src/components/todo_item/UpComingTodoItem.js
@@ -2,49 +2,64 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment'; // used to format the date from mongodb
-import { deleteTodoItem } from '../../actions/itemActions';
+import { deleteUpcomingTodoItem, setTodoItemAsCompleted } from '../../actions/itemActions';
 import EditTodoItem from './EditTodoItem';
 
 class UpComingTodoItem extends Component {
 
     onDeleteClick(id) {
-        this.props.deleteTodoItem(id);
+        this.props.deleteUpcomingTodoItem(id);
+    }
+
+    onChange(id, nameValue, descriptionValue, isCompletedValue) {
+        const newItemData = {
+            name: nameValue,
+            description: descriptionValue,
+            isCompleted: !isCompletedValue
+        };
+        this.props.setTodoItemAsCompleted(id, newItemData);
     }
 
     render() {
-        const upcomingTodos = this.props.upcomingTodos.map(upcomingTodos => ( // TODO: Found profile problems and need to resolve this. till so far not problem
-            <div key={upcomingTodos._id} className="list-group">
-                <a href="#/" className="list-group-item list-group-item-action flex-column align-items-start">
-                    <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="defaultUnchecked" />
-                                <label className="custom-control-label" htmlFor="defaultUnchecked">{upcomingTodos.name}</label>
-                            </div>
-                        </h5>
-                        <small>
-                            <div className="row">
-                                <EditTodoItem _id={upcomingTodos._id} name={upcomingTodos.name} description={upcomingTodos.description} />{' '}
-                                <div className="col-md-6">
-                                    <button className="btn btn-danger btn-sm custom-button-width" onClick={this.onDeleteClick.bind(this, upcomingTodos._id)}>
-                                        <i className="fa fa-trash" />
-                                    </button>
+        var upcomingTodos;
+        if (this.props.upcomingTodos.length > 0) {
+            upcomingTodos = this.props.upcomingTodos.map(upcomingTodos => ( // TODO: Found profile problems and need to resolve this. till so far not problem
+                <div key={upcomingTodos._id} className="list-group">
+                    <a href="#/" className="list-group-item list-group-item-action flex-column align-items-start">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">
+                                <div className="custom-control custom-checkbox">
+                                    <input type="checkbox" className="custom-control-input" id={upcomingTodos._id} checked={upcomingTodos.isCompleted} onChange={this.onChange.bind(this, upcomingTodos._id, upcomingTodos.name, upcomingTodos.description, upcomingTodos.isCompleted)} />
+                                    <label className="custom-control-label" htmlFor={upcomingTodos._id}>{upcomingTodos.name}</label>
                                 </div>
-                            </div>
+                            </h5>
+                            <small>
+                                <div className="row">
+                                    <EditTodoItem _id={upcomingTodos._id} name={upcomingTodos.name} description={upcomingTodos.description} isCompleted={upcomingTodos.isCompleted} />{' '}
+                                    <div className="col-md-6">
+                                        <button className="btn btn-danger btn-sm custom-button-width" onClick={this.onDeleteClick.bind(this, upcomingTodos._id)}>
+                                            <i className="fa fa-trash" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </small>
+                        </div>
+                        <p className="mb-1">{upcomingTodos.description}</p>
+                        <small>
+                            <Moment fromNow>{upcomingTodos.date}</Moment>
                         </small>
-                    </div>
-                    <p className="mb-1">{upcomingTodos.description}</p>
-                    <small>
-                        <Moment fromNow>{upcomingTodos.date}</Moment>
-                    </small>
-                </a>
-            </div >
-        ));
+                    </a>
+                </div >
+            ));
+        } else {
+            upcomingTodos = (
+                <p className="mb-1">You have no upcoming tasks to do. Please create one.</p>
+            );
+        }
 
         return (
             <div className="mb-4">
                 <h4>Upcoming Tasks</h4>
-                <p className="mb-1">You have no upcoming tasks to do.</p>
                 {upcomingTodos}
             </div>
         );
@@ -52,7 +67,8 @@ class UpComingTodoItem extends Component {
 }
 
 UpComingTodoItem.propTypes = {
-    deleteTodoItem: PropTypes.func.isRequired
+    deleteUpcomingTodoItem: PropTypes.func.isRequired,
+    setTodoItemAsCompleted: PropTypes.func.isRequired
 };
 
-export default connect(null, { deleteTodoItem })(UpComingTodoItem);
+export default connect(null, { deleteUpcomingTodoItem, setTodoItemAsCompleted })(UpComingTodoItem);
