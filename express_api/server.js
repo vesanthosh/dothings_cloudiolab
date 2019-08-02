38eeb09a1e99feacd1c1c9090a1cc3f9ebfe61c5
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-08-02 23:03:41 +0200

    - frontend container will serve static content

--- express_api/server.js
+++ express_api/server.js
@@ -35,16 +35,16 @@ app.use('/api/user', user);
 app.use("/api/profile", profile);
 app.use("/api/todoItem", todoItem);
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  // redirecting routes to the default page.
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   // redirecting routes to the default page.
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 9001;
 app.listen(port, () => console.log("Server started on port " + port));
